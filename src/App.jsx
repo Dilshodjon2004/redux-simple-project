c0349d16022885bcc28e28a13a7242540035bee8
Dilshodbek <dilshodjongulomov53@gmail.com>
2023-10-14 00:57:38 +0500

    Initial state of project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import { Main, Login, Register } from "./components";
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
