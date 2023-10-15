2b9e4bb13b64ed84b3de74e58d0cc9b0ee898daa
Dilshodbek <dilshodjongulomov53@gmail.com>
2023-10-16 02:53:23 +0500

    Axios intercaptors

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
 import { Routes, Route } from "react-router-dom";
 import { Main, Login, Register, Navbar } from "./components";
+import { useEffect } from "react";
+import AuthService from "./service/auth";
+import { useDispatch } from "react-redux";
+import { signUserSuccess } from "./slice/auth";
+import { getItem } from "./helpers/persistence-storage";
 function App() {
+  const dispatch = useDispatch();
+
+  const getUser = async () => {
+    try {
+      const response = await AuthService.getUser();
+      dispatch(signUserSuccess(response.user));
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const token = getItem("token");
+    if (token) {
+      getUser();
+    }
+  }, []);
   return (
     <div>
       <Navbar />
